1d30244e1c4abd89457be85c93fce124b5264fe9
Raymond <raymondurrutia2@yahoo.com>
2024-09-16 15:11:10 -0400

    Built out hero and footer sections, and created button component

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,7 +1,20 @@
+import { Fugaz_One } from 'next/font/google';
 import React from 'react'
+import Button from './Button';
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function Hero() {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <div className='py-10 sm:py-14 md:py-20 text-center'>
+            <h1 className={`text-2xl sm:text-3xl md:text-4xl ${fugaz.className}`}>
+                With <span className='textGradient'>Daily Vibez</span> you can discover <span className='textGradient'>patterns</span> and gain <span className='textGradient'>insights</span> into your emotional well-being.
+            </h1>
+            <p className='text-lg sm:text-xl md:text-2xl mt-2 mx-auto max-w-[800px]'>Keep track of your moods effortlessly, every day.</p>
+            <div className='grid grid-cols-2 gap-4 w-fit mx-auto mt-8'>
+                <Button text="Sign Up" />
+                <Button text="Login" dark />
+            </div>
+        </div>
+    )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Fugaz_One, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
   title: "DailyVibez",
@@ -9,12 +10,25 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  const Header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={`text-base sm:text-lg textGradient ${fugaz.className}`}>Daily Vibez</h1>
+    </header>
+  )
+
+  const Footer = (
+    <footer className="p-4 sm:p-8 grid place-items-center">
+      <p>&copy;{new Date().getFullYear()} <span className={`text-indigo-500 ${fugaz.className}`}>Built with</span> ❤️</p>
+    </footer>
+  )
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header>Header</header>
+      <body className={`w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ${inter.className}`}>
+        {Header}
         {children}
-        <footer>Footer</footer>
+        {Footer}
       </body>
     </html>
   );
