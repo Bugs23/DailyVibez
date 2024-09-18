56a49943bee9faf409eb49dff6ea545f4b1b929d
Raymond <raymondurrutia2@yahoo.com>
2024-09-18 19:59:19 -0400

    Built out scaffolding for login page and added statuses section to the dashboard page

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,7 +1,28 @@
+import { Fugaz_One } from 'next/font/google';
 import React from 'react'
 
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
+
 export default function Dashboard() {
+
+const statuses = {
+  num_days: 14,
+  time_remaining: "13:14:26",
+  date: (new Date()).toDateString()
+}
+
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col flex-1'>
+      <div className='grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500'>
+        {Object.keys(statuses).map((status, statusIndex) => {
+          return (
+            <div key={statusIndex} className='p-4'>
+              <p className='font-medium uppercase text-xs sm:text-sm'>{status.replaceAll("_", " ")}</p>
+              <p className={`text-base sm:text-lg ${fugaz.className}`}>{statuses[status]}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
