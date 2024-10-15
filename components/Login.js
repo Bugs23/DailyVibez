63380b303fa0b100b8cf273e206c730542b3c848
Raymond <raymondurrutia2@yahoo.com>
2024-10-15 11:56:06 -0400

    Add rule to disable react/no-unescaped-entities

--- components/Login.js
+++ components/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4">
             <h3 className={`text-4xl sm:text-5xl md:text-6xl ${fugaz.className}`}>Log In / Register</h3>
-            <p>You"re so close!</p>
+            <p>You're so close!</p>
             <input className="w-full max-w-[400px] mx-auto px-3 py-2 sm:py-3 border border-solid border-indigo-400 duration-200 hover:border-indigo-700 focus:border-indigo-700 rounded-full outline-none" placeholder="Email" />
             <input className="w-full max-w-[400px] mx-auto px-3 py-2 sm:py-3 border border-solid border-indigo-400 duration-200 hover:border-indigo-700 focus:border-indigo-700 rounded-full outline-none" placeholder="Password" type="password" />
             <div className="max-w-[400px] w-full mx-auto">
