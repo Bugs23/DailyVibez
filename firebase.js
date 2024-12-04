f94c19c25bcc907c8e2fc97cbf8e7cf8fee23676
Raymond <raymondurrutia2@yahoo.com>
2024-12-03 19:30:41 -0500

    Move firebaseConfig credentials to separate file

--- firebase.js
+++ firebase.js
@@ -2,23 +2,10 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
+import { firebaseConfig } from "./firebase.config";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-console.log("API Key:", process.env.NEXT_PUBLIC_API_KEY);
-console.log("Auth Domain:", process.env.NEXT_PUBLIC_AUTH_DOMAIN);
-console.log("Project ID:", process.env.NEXT_PUBLIC_PROJECT_ID);
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
